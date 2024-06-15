d08155b1575412c8f712c466f33b4b25dd325da6
Finco25 <165635709+Finco25@users.noreply.github.com>
2024-06-15 16:00:29 +0200

    ajout d'un embed pour join et leave

--- member.js
+++ member.js
@@ -1,29 +1,41 @@
-//Notification when a member joins and add un role the member
+const { EmbedBuilder } = require('discord.js')
+
+//Notificaiton quand un joueur rejoint le serveur 
 module.exports.joinMember = async function(member) {
-    const roleIds = ['1250310025751035925', '1250330038423916575']; 
-    const welcomeChannelId = '1250266955538108457'; 
-    const roles = roleIds.map(roleId => member.guild.roles.cache.get(roleId));
+    const avatarURL = member.user.displayAvatarURL()
+    const roleIds = ['1250310025751035925', '1250330038423916575']
+    const welcomeChannelId = '1251533493515718727'
+    const roles = roleIds.map(roleId => member.guild.roles.cache.get(roleId))
     for (const role of roles) {
         if (role) {
             try {
-                await member.roles.add(role);
-            } catch (error) {
-            }
-        } else {;
+                await member.roles.add(role); } catch (error) {}
+        } else {
         }
     }
-    const channel = member.guild.channels.cache.get(welcomeChannelId);
+    const channel = member.guild.channels.cache.get(welcomeChannelId)
     if (!channel) {
-        return;
+        return
     }
-    channel.send(`${member} viens de rejoindre le discord`); 
+    const addEmbed = new EmbedBuilder()
+        .setColor(0x1FFF00)
+        .setTitle('Bienvenue à toi sur le serveur')  
+        .setThumbnail(avatarURL)
+        .setDescription(`${ member }`)
+    channel.send({ embeds: [addEmbed] })
 }
-//Notification when a member leaves 
+//Notificaiton quand un membre quitte le serveur 
 module.exports.leaveMember = function(member) {
-    const leaveChannelId = '1250266955538108457'; 
-    const channel = member.guild.channels.cache.get(leaveChannelId);
+    const avatarURL = member.user.displayAvatarURL()
+    const leaveChannelId = '1251533493515718727'
+    const channel = member.guild.channels.cache.get(leaveChannelId)
     if (!channel) return;
-    channel.send(`${member} viens de quitter le discord`);
+    const addEmbed = new EmbedBuilder()
+        .setColor(0xFF0000)
+        .setTitle('Pourquoi tu quittes')  
+        .setThumbnail(avatarURL)
+        .setDescription(`${ member }`)
+    channel.send({ embeds: [addEmbed] })
 }
 
 
